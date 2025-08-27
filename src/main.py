d9a249e9040e8067549de29d06612dee74339587
Cyrrian <william.r.stewart@gmail.com>
2025-08-27 08:30:08 -0500

    setup basic frameowrk

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    text_node = TextNode('This is some anchor text', TextType.TEXT_LINK, 'https://www.boot.dev')
+    print(text_node)
+
+if __name__ == "__main__":
+    main()
