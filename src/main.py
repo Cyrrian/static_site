f80d3a942cd79cb3363a61a96ce0dbf3592cfa2e
Cyrrian <william.r.stewart@gmail.com>
2025-09-10 12:38:47 -0500

    added setup of public dir

--- src/main.py
+++ src/main.py
@@ -1,9 +1,34 @@
+import os, shutil
+
 from textnode import TextNode, TextType
 
 
 def main():
-    text_node = TextNode('This is some anchor text', TextType.TEXT_LINK, 'https://www.boot.dev')
-    print(text_node)
+    public_dir = 'public'
+    static_dir = 'static'
+
+    clear_public(public_dir)
+    copy_files(static_dir, public_dir)
+
+
+def clear_public(dir):
+    
+    if os.path.exists(dir):
+        shutil.rmtree(dir)
+    os.mkdir(dir)
+
+def copy_files(dir, target):
+    dir_content = os.listdir(dir)
+    for item in dir_content:
+        item_path = os.path.join(dir, item)
+        target_path = os.path.join(target, item)
+        if os.path.isdir(item_path):
+            print(f'DIR {item_path}')
+            os.mkdir(target_path)
+            copy_files(item_path, target_path)
+        else:
+            print(f'FILE {item_path}')
+            shutil.copy(item_path, target_path)
 
 if __name__ == "__main__":
     main()
